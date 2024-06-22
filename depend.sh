d07719ac1315e0f6448a749e65f51e8356d0fc02
gerben <gerben@0m.dev>
2024-06-22 01:55:04 -0700

    fix depend

--- depend.sh
+++ depend.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
-DIR=$1
-shift 1
+CC=$1
+DIR=$2
+shift 2
 case "$DIR" in
 "" | ".")
-i686-linux-gnu-g++ -MM -MG -c "$@" | sed -e 's@^\(.*\).o:@\1.d \1.o:@g'
+$CC -MM -MG -c "$@" | sed -e 's@^\(.*\).o:@\1.d \1.o:@g'
 ;;
 *)
-i686-linux-gnu-g++ -MM -MG -c "$@" | sed -e "s@^\(.*\).o:@$DIR\/\1.d $DIR\/\1.o:@g"
+$CC -MM -MG -c "$@" | sed -e "s@^\(.*\).o:@$DIR\/\1.d $DIR\/\1.o:@g"
 ;;
 esac
